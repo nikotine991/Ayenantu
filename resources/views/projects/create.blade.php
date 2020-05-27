83c59f57b1e5ac93252aed0a050594ed59e03c0c
Nikotine <nikotine991@gmail.com>
2020-05-27 12:41:15 -0400

    se crea vistas _form y validate-errors para reutilizar codigo

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -5,33 +5,14 @@
 @section('content')
     <h1>Crear proyecto</h1>
 
-    @if($errors->any())
-        <ul>
-            @foreach($errors->all() as $error)
-            <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    @endif 
+    @include('partials.validation-errors')
 
     <form method="POST" action="{{ route('projects.store')}}">
         @csrf
-        <label>
-            Titulo del proyecto <br>
-            <input type="text" name="title">
-        </label>
-        <br>
-        <label>
-            URL del proyecto <br>
-            <input type="text" name="url">
-        </label>
-        <br>
-        <label>
-            Descripcion del proyecto <br>
-            <textarea name="description"></textarea>
-        </label>
-        <br>
+        
+        @include('projects._form')
+
         <button>Guardar</button>
     </form>
 
- 
 @endsection
